5018444fcee5fcbf8b81963ca8036c38b92a13ce
André Calza <andre.calza@navegarti.com.br>
2017-10-19 16:17:16 -0200

    Dev

---
+++ scripts/js/config_firebase.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDQoAATpljY3OitVyoa3HjjbM0_xQX5ysc",
+    authDomain: "reactfodation.firebaseapp.com",
+    databaseURL: "https://reactfodation.firebaseio.com",
+    projectId: "reactfodation",
+    storageBucket: "",
+    messagingSenderId: "320277307513"
+  };
+  firebase.initializeApp(config);
